f315f6937710a539b25e461260a36830cda154a1
mazy99 <mazy.1999@mail.ru>
2026-06-04 15:28:55 +0300

    Добавлены описания методов класса

--- serial_core/serial_manage.py
+++ serial_core/serial_manage.py
@@ -7,8 +7,11 @@ class SerialManager:
         self.serial_port = None
 
 
+
     @staticmethod
     def scan_serial_ports() -> list:
+
+        '''Получение списка доступных COM-портов на компьютере'''
 
         ports: list = serial.tools.list_ports.comports()
 
@@ -17,12 +20,17 @@ class SerialManager:
     @staticmethod
     def get_default_port() -> str | None:
 
+        '''Получение первого доступного COM-порта, если он есть'''
+
         ports = SerialManager.scan_serial_ports()
 
         return ports[0] if ports else None
     
 
     def connect(self, config) -> bool:
+
+        '''Установка соединения с COM-портом на основе переданных параметров конфигурации'''
+
         try:
             self.serial_port = serial.Serial(
                 port=config.port,
@@ -45,6 +53,9 @@ class SerialManager:
             return False
 
     def disconnect(self) -> bool:
+
+        '''Закрытие соединения с COM-портом, если оно установлено'''
+
         try:
             if self.serial_port and self.serial_port.is_open:
                 self.serial_port.close()
@@ -56,6 +67,9 @@ class SerialManager:
             print(f"[ERROR] Disconnect failed: {e}")
 
     def is_connected(self) -> bool:
+
+        '''Проверка, установлено ли соединение с COM-портом'''
+        
         return  self.serial_port  is not None and self.serial_port.is_open
 
 
