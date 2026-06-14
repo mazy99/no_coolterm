2732ca10e934e411ebcc0ed445441aa2409b85aa
mazy99 <mazy.1999@mail.ru>
2026-06-14 22:18:55 +0300

    обновленный модуль для загрузки стилей из файлов

--- utils/style_loader.py
+++ utils/style_loader.py
@@ -3,16 +3,13 @@
 from pathlib import Path
 
 
-def load_stylesheet(filename="styles.qss"):
+def load_stylesheet(filename: str) -> str:
 
     try:
-        style_path = Path(__file__).parent.parent / "gui" / "main_window" / filename
-
-        if style_path.exists():
-            with open(style_path, "r") as f:
-                return f.read()
+        style_path = Path(filename)
+        with open(style_path, "r") as file:
+            return file.read()
 
     except Exception as e:
         print(f"Error loading stylesheet: {e}")
-
 
