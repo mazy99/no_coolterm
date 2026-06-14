a8be3225885790248638f850b86ba90bbc24831b
4isto <4istopopikoly@vk.com>
2026-06-14 16:04:33 +0300

    version 2.0

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building ModbusTerminal...
-cd src
-python -m PyInstaller --onefile --windowed --name="ModbusTerminal" main.py
-copy /Y dist\ModbusTerminal.exe ..\
-cd ..
-echo Done!
-pause
--- src/modbus_core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .modbus_engine import ModbusEngine
-from .serial_manager import SerialManager, ModbusConfig
-from .modbus_rtu import ModbusRTU
-
-__all__ = ['ModbusEngine', 'SerialManager', 'ModbusConfig', 'ModbusRTU']
