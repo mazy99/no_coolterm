41735f0d6a0694e6a53c512bd0c68f5e6281cda5
mazy99 <mazy.1999@mail.ru>
2026-06-25 13:11:06 +0300

    загрузка иконки при запуске программы

--- main_gui.py
+++ main_gui.py
@@ -1,10 +1,17 @@
 """Точка входа в приложение"""
 
 import sys
+import ctypes
 from PyQt6.QtWidgets import QApplication
 from gui.gui import ModbusTerminal
 
 def main():
+    
+    try:
+        myappid = 'mycompany.modbusterminal.nocoolterm.1.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except Exception as e:
+        print(f"Не удалось установить AppUserModelID: {e}")
     app = QApplication(sys.argv)
     window = ModbusTerminal()
     window.show()
