f091049fdeebaac8c3de89a2ba04b9e194a8bd61
mazy99 <mazy.1999@mail.ru>
2026-06-25 13:10:27 +0300

    обновлена утилита для загрузки стилей

--- utils/style_loader.py
+++ utils/style_loader.py
@@ -1,13 +1,6 @@
-import os
-import sys
 
-def get_resource_path(relative_path):
-    """ Возвращает абсолютный путь к ресурсу, учитывая сборку PyInstaller """
-    if hasattr(sys, '_MEIPASS'):
-        # Если запущено внутри .exe, PyInstaller распаковывает всё в sys._MEIPASS
-        return os.path.join(sys._MEIPASS, relative_path)
-    # Если запущено в режиме разработки из папки проекта
-    return os.path.join(os.path.abspath("."), relative_path)
+
+from .get_resource_path import get_resource_path
 
 def load_stylesheet(path_to_qss):
     """ Функция загрузки стилей, которая теперь не ломается в .exe """
