c30a99602e197b54d3bdd498af308006624bbb4d
mazy99 <mazy.1999@mail.ru>
2026-06-29 13:38:07 +0300

    отформатирован файл

--- utils/style_loader.py
+++ utils/style_loader.py
@@ -1,12 +1,11 @@
-
-
 from .get_resource_path import get_resource_path
 
+
 def load_stylesheet(path_to_qss):
-    """ Функция загрузки стилей, которая теперь не ломается в .exe """
+    """Функция загрузки стилей, которая теперь не ломается в .exe"""
     # Перенаправляем относительный путь через наш обработчик
     resolved_path = get_resource_path(path_to_qss)
-    
+
     try:
         with open(resolved_path, "r", encoding="utf-8") as f:
             return f.read()
