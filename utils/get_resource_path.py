bfe91e6856f22444428a8833d87d63e2a2bce08d
mazy99 <mazy.1999@mail.ru>
2026-06-29 13:37:31 +0300

    отформатирован файл

--- utils/get_resource_path.py
+++ utils/get_resource_path.py
@@ -1,11 +1,9 @@
-
-
 import os
 import sys
 
 
 def get_resource_path(path):
 
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, path)
     return os.path.abspath(path)
