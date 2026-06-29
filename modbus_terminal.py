1e0ce33ed3e598f915aaabac9e28ef5076ff3a22
mazy99 <mazy.1999@mail.ru>
2026-06-29 12:49:21 +0300

    изменено название основного файла

---
+++ modbus_terminal.py
@@ -0,0 +1,21 @@
+"""Точка входа в приложение"""
+
+import sys
+import ctypes
+from PyQt6.QtWidgets import QApplication
+from gui.gui import ModbusTerminal
+
+def main():
+    
+    try:
+        myappid = 'mycompany.modbusterminal.nocoolterm.1.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except Exception as e:
+        print(f"Не удалось установить AppUserModelID: {e}")
+    app = QApplication(sys.argv)
+    window = ModbusTerminal()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
